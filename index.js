916d1ffc9c0d55ab319da589cbe357a42f557989
sharon884 <sharonsharon9048461583@gmail.com>
2025-12-08 16:20:16 +0530

    how to create a http server

---
+++ index.js
@@ -0,0 +1,18 @@
+// const http = require("http");
+
+// const server = http.createServer(( req,res ) => {
+//     if ( req.url == "/") {
+//         res.end("helowfrom http server");
+//     }else if ( req.url === "/hai" ) {
+//         res.end("hai from http server");
+//     };
+
+
+  
+// });
+
+
+
+// server.listen(3001,() => {
+//     console.log("server is listening on port http://localhost:3001")
+// })
