8b5fd36344fc8bff21a5c24e4066e4b8b29b0f80
sharon884 <sharonsharon9048461583@gmail.com>
2025-12-08 16:50:16 +0530

    created express sever

--- index.js
+++ index.js
@@ -16,3 +16,20 @@
 // server.listen(3001,() => {
 //     console.log("server is listening on port http://localhost:3001")
 // })
+
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Home route");
+});
+
+app.get("/hai", (req, res) => {
+    res.send("Hai route");
+});
+
+
+app.listen(3002,() => {
+    console.log("sever is running http://localhost:3002");
+});
