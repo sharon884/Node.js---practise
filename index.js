dcac9acb42f8094a080f269f637f0967197a797e
sharon884 <sharonsharon9048461583@gmail.com>
2025-12-11 10:03:36 +0530

    how to create a http server

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
-// const http = require("http");
+const http =  require("http");
 
-// const server = http.createServer(( req,res ) => {
-//     if ( req.url == "/") {
-//         res.end("helowfrom http server");
-//     }else if ( req.url === "/hai" ) {
-//         res.end("hai from http server");
-//     };
-
-
-  
-// });
-
-
-
-// server.listen(3001,() => {
-//     console.log("server is listening on port http://localhost:3001")
-// })
-
-
-const express = require("express");
-const app = express();
-
-const middleware = ( ( req, res , next ) => {
-     if ( req.method === "GET" ) {
-        return res.send("blocked");
-     };
-
-     next();
-});
-
-app.get("/", middleware, (req, res) => {
-    res.send("Home route");
-});
-
-app.get("/hai", (req, res) => {
-    res.send("Hai route");
+const server = http.createServer((req,res)=> {
+    if ( req.url == "/") {
+        res.end("hellow world ");
+    }else {
+        res.end("hai world");
+    }
 });
 
 
-app.listen(3002,() => {
-    console.log("sever is running http://localhost:3002");
-});
+
+
+
+server.listen(3001,() => {
+    console.log("server is listening on http://localhost:3001")
+})
